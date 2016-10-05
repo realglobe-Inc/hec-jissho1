b8f980964f46667038c564def764fff4c436399c
FujiHaruka <fujiharuka@realglobe.jp>
2016-10-05 14:36:40 +0900

    情報欄を隠せるようにした

--- ui/js/components/controller_panel.jsx
+++ ui/js/components/controller_panel.jsx
@@ -4,17 +4,36 @@
 import React from 'react'
 import ControllerPanelArea from '../containers/controller_panel_area'
 import ControllerPanelSelect from '../containers/controller_panel_select'
+import c from 'classnames'
 
 const debug = require('debug')('hec:ControllerPanel')
 
 const ControllerPanel = React.createClass({
+  getInitialState () {
+    return {
+      display: true
+    }
+  },
+
   render () {
+    const s = this
+    let {display} = s.state
+    let width = 400 // See controller-panel.scss $panel-width
+    let displayStyle = display ? {} : {left: `-${width}px`}
     return (
-      <div className='controller-panel'>
+      <div className='controller-panel' style={displayStyle}>
+        <div className='panel-display-toggle' onClick={s.toggleDisplay}>
+          <i className={c('fa', 'fa-2x', display ? 'fa-caret-left' : 'fa-caret-right')} aria-hidden></i>
+        </div>
         <ControllerPanelSelect/>
         <ControllerPanelArea/>
       </div>
     )
+  },
+
+  toggleDisplay () {
+    debug('toggle', this.state.display)
+    this.setState({display: !this.state.display})
   }
 })
 
